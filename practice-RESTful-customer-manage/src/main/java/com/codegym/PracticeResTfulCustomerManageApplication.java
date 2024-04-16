d558e8e5daae355f39ac28e6da1707ede8f0158c
Ngo Hoang <ngohoang8320@gmail.com>
2024-04-17 05:49:39 +0700

    Add practice project about using RESTfult to mange customer

---
+++ practice-RESTful-customer-manage/src/main/java/com/codegym/PracticeResTfulCustomerManageApplication.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeResTfulCustomerManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeResTfulCustomerManageApplication.class,
+                args);
+    }
+
+}
